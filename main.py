166e540d13d01cdc7f52fbbba26d4cbd1d963081
Allen95Wei <49893966+Alllen95Wei@users.noreply.github.com>
2022-02-16 16:19:08 +0800

    新增編輯訂單功能(在輸入非自己訂單的編號，會當機)

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import order_notifer
 client = discord.Client()
 localtime = time.localtime()
 final_msg = []
-time_stamp = time.strftime("%Y/%m/%d  %p %I:%M:%S", localtime)
+time_stamp = time.strftime("%Y/%m/%d  %p  %I:%M:%S", localtime)
 base_dir = os.path.abspath(os.path.dirname(__file__))
 mode = "ready"
 current_order = {}
@@ -20,7 +20,7 @@ channel = []
 
 @client.event
 async def on_ready():
-    music = discord.Activity(type=discord.ActivityType.playing, name="Alpha測試階段")
+    music = discord.Activity(type=discord.ActivityType.playing, name="Beta測試階段")
     await client.change_presence(status=discord.Status.online, activity=music)
     log_writter.write_log("-------------------------------------------------------------\n", True)
     log_writter.write_log("\n登入成功！\n目前登入身份：" +
@@ -39,7 +39,9 @@ async def on_message(message):
         if msg_in[3:] == "help":
             embed = discord.Embed(title="協助", description="本機器人正在開發中，敬請期待！", color=0x14E073)
             embed.add_field(name="`help`", value="顯示此協助訊息。")
+            embed.add_field(name="`ping`", value="檢查本機器人延遲狀態。")
             embed.add_field(name="`add`", value="新增訂單。")
+            embed.add_field(name="`edit`", value="編輯訂單。")
             embed.set_footer(text=time_stamp)
             final_msg.append(embed)
             final_msg_type.append("embed")
@@ -100,9 +102,9 @@ async def on_message(message):
             use_log = str(message.channel) + "/" + str(message.author) + ":\n" + msg_in + "\n\n"
             log_writter.write_log(use_log)
             if msg_in == "n":
-                music = discord.Activity(type=discord.ActivityType.playing, name="Alpha測試階段")
+                music = discord.Activity(type=discord.ActivityType.playing, name="Beta測試階段")
                 await client.change_presence(status=discord.Status.online, activity=music)
-                embed = discord.Embed(title="已取消訂單", description="訂單已取消！", color=0x14E073)
+                embed = discord.Embed(title="已取消操作", description="操作已取消！", color=0x14E073)
                 embed.set_footer(text=time_stamp)
                 final_msg.append(embed)
                 final_msg_type.append("embed")
@@ -110,7 +112,7 @@ async def on_message(message):
                 mode = "ready"
                 current_order = {}
             elif mode == "busy-1":
-                if msg_in.startswith("http://") or msg_in.startswith("https://"):
+                if msg_in.startswith("http"):
                     mode = "busy-2"
                     current_order["url"] = msg_in
                     embed = discord.Embed(title="新增訂單", description="已新增！請輸入**商品數量**。", color=0x14E073)
@@ -127,7 +129,7 @@ async def on_message(message):
             elif mode == "busy-2":
                 if msg_in.isdigit():
                     mode = "busy-3"
-                    current_order["amount"] = int(msg_in)
+                    current_order["amount"] = msg_in
                     embed = discord.Embed(title="新增訂單", description="已確認！請確認下方訂單內容是否正確。", color=0x14E073)
                     embed.set_footer(text=time_stamp)
                     final_msg.append(embed)
@@ -154,7 +156,7 @@ async def on_message(message):
                     channel.append(message.channel)
             elif mode == "busy-3":
                 if msg_in == "y" or "Y":
-                    music = discord.Activity(type=discord.ActivityType.playing, name="Alpha測試階段")
+                    music = discord.Activity(type=discord.ActivityType.playing, name="Beta測試階段")
                     await client.change_presence(status=discord.Status.online, activity=music)
                     order_txt_path = base_dir + "\\order-database\\" + str(current_order["order_num"]) + ".txt"
                     order_txt = open(order_txt_path, mode="a", encoding="utf-8")
@@ -173,7 +175,7 @@ async def on_message(message):
             elif mode == "busy-e-1":
                 if not msg_in.startswith("AO-"):
                     embed = discord.Embed(title="錯誤：查詢訂單", description="請輸入**訂單編號(AO-XX)**。", color=0xf21c1c)
-                    embed.set_footer(text=time_stamp)
+                    embed.set_footer(text="輸入n以取消 • " + time_stamp)
                     final_msg.append(embed)
                     final_msg_type.append("embed")
                     channel.append(message.channel)
@@ -181,28 +183,122 @@ async def on_message(message):
                     try:
                         order_txt_path = base_dir + "\\order-database\\" + msg_in + ".txt"
                         order_txt = open(order_txt_path, mode="r", encoding="utf-8")
-                        order_txt = str(order_txt.read())
-                        current_order = eval(order_txt)
+                        current_order = str(order_txt.read())
+                        current_order = eval(current_order)
+                        order_txt.close()
                         if current_order["author"] == str(message.author):
-                            embed = discord.Embed(title="訂單內容", description="訂單編號：" + str(msg_in), color=0x14E073)
+                            embed = discord.Embed(title="訂單內容", description="訂單編號：{0}".format(str(msg_in)),
+                                                  color=0x14E073)
                             embed.add_field(name=":one: 商品連結", value=current_order["url"], inline=False)
                             embed.add_field(name=":two: 商品數量", value=current_order["amount"], inline=False)
-                        else:
-                            embed = discord.Embed(title="錯誤：查詢訂單", description="訂單編號：`{0}`，並非您的訂單。".format(str(msg_in)),
-                                                  color=0xf21c1c)
                             embed.set_footer(text=time_stamp)
+                            final_msg.append(embed)
+                            final_msg_type.append("embed")
+                            channel.append(message.channel)
+                            embed = discord.Embed(title="選擇編輯欄位", description="請輸入要編輯的**欄位編號**。", color=0x14E073)
+                            embed.set_footer(text="輸入n以取消 • " + time_stamp)
+                            mode = "busy-e-2"
+                        else:
+                            embed = discord.Embed(title="錯誤：編輯訂單", description="訂單編號：`{0}`，並非您的訂單。".format(str(msg_in)),
+                                                  color=0xf21c1c)
+                            embed.set_footer(text="輸入n以取消 • " + time_stamp)
                         final_msg.append(embed)
                         final_msg_type.append("embed")
                         channel.append(message.channel)
                     except FileNotFoundError:
-                        embed = discord.Embed(title="錯誤：查詢訂單", description="訂單編號{0}不存在！".format(msg_in), color=0xf21c1c)
-                        embed.set_footer(text=time_stamp)
+                        embed = discord.Embed(title="錯誤：編輯訂單", description="訂單編號`{0}`不存在！".format(msg_in),
+                                              color=0xf21c1c)
+                        embed.set_footer(text="輸入n以取消 • " + time_stamp)
+                        final_msg.append(embed)
+                        final_msg_type.append("embed")
+                        channel.append(message.channel)
+            elif mode == "busy-e-2":
+                if msg_in.isdigit():
+                    if int(msg_in) == 1:
+                        embed = discord.Embed(title="編輯訂單：`{0}`".format(current_order["order_num"]),
+                                              description="請輸入新的商品連結。", color=0x14E073)
+                        embed.set_footer(text="輸入n以取消 • " + time_stamp)
+                        final_msg.append(embed)
+                        final_msg_type.append("embed")
+                        channel.append(message.channel)
+                        mode = "busy-e-3"
+                    elif int(msg_in) == 2:
+                        embed = discord.Embed(title="編輯訂單：`{0}`".format(current_order["order_num"]),
+                                              description="請輸入新的商品數量。", color=0x14E073)
+                        embed.set_footer(text="輸入n以取消 • " + time_stamp)
+                        final_msg.append(embed)
+                        final_msg_type.append("embed")
+                        channel.append(message.channel)
+                        mode = "busy-e-4"
+                    else:
+                        embed = discord.Embed(title="錯誤：編輯訂單", description="請輸入**欄位編號(1或2)**。", color=0xf21c1c)
+                        embed.set_footer(text="輸入n以取消 • " + time_stamp)
+                        final_msg.append(embed)
+                        final_msg_type.append("embed")
+                        channel.append(message.channel)
+                else:
+                    embed = discord.Embed(title="錯誤：編輯訂單", description="請輸入**欄位編號(1或2)**。", color=0xf21c1c)
+                    embed.set_footer(text="輸入n以取消 • " + time_stamp)
+                    final_msg.append(embed)
+                    final_msg_type.append("embed")
+                    channel.append(message.channel)
+            elif mode == "busy-e-3":
+                if msg_in.startswith("http"):
+                    current_order["url"] = msg_in
+                    embed = discord.Embed(title="編輯訂單：`{0}`".format(current_order["order_num"]),
+                                          description="已經編輯了商品連結。", color=0x14E073)
+                    embed.add_field(name="商品連結", value=current_order["url"], inline=False)
+                    embed.add_field(name="商品數量", value=current_order["amount"], inline=False)
+                    embed.set_footer(text=time_stamp)
+                    final_msg.append(embed)
+                    final_msg_type.append("embed")
+                    channel.append(message.channel)
+                    order_txt_path = base_dir + "\\order-database\\" + str(current_order["order_num"]) + ".txt"
+                    order_txt = open(order_txt_path, mode="w", encoding="utf-8")
+                    order_txt.write(str(current_order))
+                    order_txt.close()
+                    music = discord.Activity(type=discord.ActivityType.playing, name="Beta測試階段")
+                    await client.change_presence(status=discord.Status.online, activity=music)
+                    mode = "ready"
+                    order_info = order_notifer.new_order_notice(current_order["order_num"], True)
+                else:
+                    embed = discord.Embed(title="錯誤：編輯訂單", description="請輸入正確的**商品連結**。", color=0xf21c1c)
+                    embed.set_footer(text="輸入n以取消 • " + time_stamp)
+                    final_msg.append(embed)
+                    final_msg_type.append("embed")
+                    channel.append(message.channel)
+            elif mode == "busy-e-4":
+                if msg_in.isdigit():
+                    current_order["amount"] = msg_in
+                    embed = discord.Embed(title="編輯訂單：`{0}`".format(current_order["order_num"]),
+                                          description="已經編輯了商品數量。", color=0x14E073)
+                    embed.add_field(name="商品連結", value=current_order["url"], inline=False)
+                    embed.add_field(name="商品數量", value=current_order["amount"], inline=False)
+                    embed.set_footer(text=time_stamp)
+                    final_msg.append(embed)
+                    final_msg_type.append("embed")
+                    channel.append(message.channel)
+                    order_txt_path = base_dir + "\\order-database\\" + str(current_order["order_num"]) + ".txt"
+                    order_txt = open(order_txt_path, mode="w", encoding="utf-8")
+                    order_txt.write(str(current_order))
+                    order_txt.close()
+                    music = discord.Activity(type=discord.ActivityType.playing, name="Beta測試階段")
+                    await client.change_presence(status=discord.Status.online, activity=music)
+                    mode = "ready"
+                    order_info = order_notifer.new_order_notice(current_order["order_num"], True)
+                else:
+                    embed = discord.Embed(title="錯誤：編輯訂單", description="請輸入正確的**商品數量**。", color=0xf21c1c)
+                    embed.set_footer(text="輸入n以取消 • " + time_stamp)
+                    final_msg.append(embed)
+                    final_msg_type.append("embed")
+                    channel.append(message.channel)
     if order_info != "":
         final_msg.append("<@657519721138094080>")
         final_msg_type.append("normal")
         channel.append(client.get_channel(942379971345797151))
         final_msg.append(order_info)
         final_msg_type.append("embed")
+        channel.append(client.get_channel(942379971345797151))
         channel.append(message.channel)
         order_info = ""
     for i in range(len(final_msg)):
